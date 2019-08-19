f7a6480beecc180beadc34b5f2d366d099eafa14
patrickbrown <patricksbrown1992@gmail.com>
2019-08-19 17:05:52 -0400

    functional anagrams

--- ruby_problems.rb
+++ ruby_problems.rb
@@ -38,4 +38,15 @@ def largest_contiguous_subsum(arr)
 end
 
 
-p largest_contiguous_subsum([2, 3, -6, 7, -6, 7])
+def anagrams(str1, str2)
+    hash = Hash.new(0)
+    str1.each_char do |char|
+        hash[char] += 1
+    end
+
+    str2.each_char do |char2|
+        hash[char2] -= 1
+    end
+
+    hash.values.all? {|num| num == 0}
+end
