560eb018b73f231339bd34a6af7c0cfac7125d0c
patrickbrown <patricksbrown1992@gmail.com>
2019-08-19 16:50:49 -0400

    functional two sum

---
+++ ruby_problems.rb
@@ -0,0 +1,26 @@
+require 'byebug'
+
+def two_sum(array)
+    hash = Hash.new
+    ans = []
+    array.each_with_index do |num, i|
+        hash[num] = i
+        difference = 0 - num
+        ans << [hash[difference], i] if hash[difference] && num != 0
+    end
+
+    ans
+end
+
+def my_min(array)
+    ans = nil
+    array.each do |num|
+        if ans == nil || num < ans
+            ans = num
+        end
+    end
+    ans
+
+end
+
+p my_min([-1, 0, 2, -2, 1])
