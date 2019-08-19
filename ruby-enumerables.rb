f069232e3c12740a23ad38e519d5d08321822ff2
patrickbrown <patricksbrown1992@gmail.com>
2019-08-19 16:28:04 -0400

    functional my any

--- ruby-enumerables.rb
+++ ruby-enumerables.rb
@@ -23,14 +23,18 @@ class Array
     self.my_each do |item|
         ans << item if !prc.call(item)
     end
-
     ans
+  end
 
- 
+  def my_any?(&prc)
+    self.my_each do |item|
+      return true if prc.call(item)
+    end
+    false
   end
 
 end 
 
 
 
-p [1,2,4].my_reject {|x| x > 1}
+p [1,2,4].my_any? {|x| x > 5}
